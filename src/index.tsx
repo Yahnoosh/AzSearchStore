3c93715c7f651c2fc8994c4000cdb326da4567a0
unknown <evboyle@mrroboto.redmond.corp.microsoft.com>
2017-03-06 08:25:20 -0800

    updating example with a simple react component

--- src/index.tsx
+++ src/index.tsx
@@ -10,11 +10,15 @@ import * as searchParameterActions from "./actions/searchParametersActions";
 import * as facetsActions from "./actions/facetsActions";
 import * as promise from "es6-promise";
 
+
+// create store
 const store = redux.createStore(
     reducers,
     {} as Store.SearchState,
     redux.applyMiddleware(ReduxThunk)
 );
+
+// subscribe and dump state transitions to console
 store.subscribe(() => {
     const state = store.getState();
     console.info("--------------------state--------------------");
@@ -25,16 +29,55 @@ store.subscribe(() => {
 
 });
 
+// configuration
 store.dispatch(configActions.setConfig({ index: "wikiversity", queryKey: "4412747C72BF48B6C761ED7E00D9964D", service: "azsdoofgod" }));
-store.dispatch(searchParameterActions.updateParameters({count: true}));
+store.dispatch(searchParameterActions.updateParameters({ count: true }));
 store.dispatch(searchParameterActions.setInput("*"));
 store.dispatch(facetsActions.addCheckboxFacet("campusType", false));
 store.dispatch(facetsActions.addRangeFacet("studentsCount", 0, 100000));
 store.dispatch(facetsActions.addRangeFacet("endowmentAmount", 0, 5000000000));
 
-store.dispatch(asyncActions.fetchSearchResults).then(() => {
-    store.dispatch(facetsActions.toggleCheckboxFacetSelection("campusType", "urban"))
-    store.dispatch(asyncActions.fetchSearchResultsFromFacet);
-});
+
+interface SearchProps {
+    store: redux.Store<Store.SearchState>;
+}
 
 
+class SearchPage extends React.Component<SearchProps, Store.SearchState> {
+    componentDidMount() {
+        this.props.store.subscribe(() => { this.setState(this.props.store.getState()); });
+    }
+    constructor(props: SearchProps) {
+        super(props);
+        // set initial state
+        this.state = props.store.getState();
+    }
+    handleChange(event: any) {
+        this.props.store.dispatch(searchParameterActions.setInput(event.target.value));
+    }
+    onKeyPress(event: any) {
+        if (event.key === "Enter") {
+            this.props.store.dispatch(asyncActions.fetchSearchResults);
+        }
+    }
+    render() {
+        const state = this.state;
+        const results: any = this.state.results.results;
+        if (!state) {
+            return <div />;
+        }
+        return (
+            <div>
+                <input value={state.searchParameters.input} onChange={this.handleChange.bind(this)} onKeyPress={this.onKeyPress.bind(this)}></input>
+                <h3>count: {state.results.count}</h3>
+                <ul>
+                    {results.map((result: any) => {
+                        return <li>{result.title}</li>;
+                    })}
+                </ul>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<SearchPage store={store} />, document.getElementById("example"));
