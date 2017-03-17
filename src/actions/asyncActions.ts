67d19f9ffb7685ef95a9b26312a5c747477b36bf
unknown <evboyle@mrroboto.redmond.corp.microsoft.com>
2017-03-16 20:07:30 -0700

    set user agent when making network calls

--- src/actions/asyncActions.ts
+++ src/actions/asyncActions.ts
@@ -9,6 +9,7 @@ import { buildSearchURI, buildSuggestionsURI, buildPostBody, suggestParameterVal
 promise.polyfill();
 import thunk, { ThunkAction } from "redux-thunk";
 
+const userAgent = "AzSearchStore/Preview";
 
 const searchAndDispatch: ThunkAction<Promise<void>, Store.SearchState, {
     resultsActionToDispatch: (results: {}[], recievedAt: number, count?: number) => resultsActions.ResultsAction,
@@ -24,7 +25,8 @@ const searchAndDispatch: ThunkAction<Promise<void>, Store.SearchState, {
         const postBody = buildPostBody(parameters.searchParameters, parameters.input, searchParameterValidator, searchState.facets);
         let headers = new Headers({
             "api-key": searchState.config.queryKey,
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "User-Agent": userAgent
         });
         dispatch(resultsActions.initiateSearch());
         const promise = searchCallback ? searchCallback(searchState, postBody) :
@@ -73,7 +75,8 @@ export const suggest: ThunkAction<Promise<void>, Store.SearchState, {}> =
         const postBody = buildPostBody(parameters.suggestionsParameters, parameters.input, suggestParameterValidator);
         let headers = new Headers({
             "api-key": searchState.config.queryKey,
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "User-Agent": userAgent
         });
         dispatch(suggestionsActions.initiateSuggest());
         const promise = suggestCallBack ? suggestCallBack(searchState, postBody) :
